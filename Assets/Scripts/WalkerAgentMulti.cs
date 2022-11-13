e14158aa978badaf65df2a55bcd786c2f5532580
Toader Carausu <tcarausu@gmail.com>
2022-11-13 15:45:53 +0100

    adjusted the scripts + added tag check on goal (#2)

--- Assets/Scripts/WalkerAgentMulti.cs
+++ Assets/Scripts/WalkerAgentMulti.cs
@@ -1,22 +1,33 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WalkerAgentMulti : Agent
 {
     CharacterController controller;
     private List<Checkpoint> clearedCheckpoints;
 
-    void Start () 
+    private Vector3 initialPosition;
+    private Rigidbody rb;
+    private  Vector3 spawnSize;
+
+    void Start()
     {
         controller = GetComponent<CharacterController>();
         clearedCheckpoints = new List<Checkpoint>();
+
+        initialPosition = transform.localPosition;
+        rb = GetComponent<Rigidbody>();
+         spawnSize = SpawnArea.GetComponent<Renderer>().bounds.size;
+
     }
 
-    public void checkpoint(Checkpoint cp){
+    public void checkpoint(Checkpoint cp)
+    {
         AddReward(0.2f);
         clearedCheckpoints.Add(cp);
     }
@@ -32,35 +43,49 @@ public class WalkerAgentMulti : Agent
 
     public Transform SpawnArea;
     public Transform Target;
+
     public override void OnEpisodeBegin()
     {
-        foreach(Checkpoint cp in clearedCheckpoints){
+        foreach (Checkpoint cp in clearedCheckpoints)
+        {
             cp.SetActive(true);
         }
+
         clearedCheckpoints.Clear();
         RndSpawn();
     }
 
-    public void RndSpawn(){
-        Vector3 spawnSize = SpawnArea.GetComponent<Renderer>().bounds.size;
-        Vector3 rndPosition = new Vector3( Random.value * spawnSize.x - spawnSize.x/2, 1, Random.value * spawnSize.z - spawnSize.z/2);
-        if(rndPosition.x > 0)
+    private void RndSpawn()
+    {
+        //todo aless code
+        // Vector3 rndPosition = new Vector3(Random.value * spawnSize.x - spawnSize.x / 2, 1,
+        // Random.value * spawnSize.z - spawnSize.z / 2);
+
+        var randomV = Random.value;
+        Vector3 rndPosition = new Vector3(
+            randomV * spawnSize.x - spawnSize.x / 2,
+            initialPosition.y,
+            randomV * spawnSize.z - spawnSize.z / 2);
+
+        if (rndPosition.x > 0)
             rndPosition.x -= 1;
-        else 
+        else
             rndPosition.x += 1;
-        
-        if(rndPosition.z > 0)
+
+        if (rndPosition.z > 0)
             rndPosition.z -= 1;
-        else 
+        else
             rndPosition.z += 1;
 
-        this.transform.localPosition = rndPosition;
-        Debug.Log(rndPosition);
+        transform.localPosition = rndPosition;
+        // Debug.Log(rndPosition);
     }
 
-    public void MoveTarget(){
+    private void MoveTarget()
+    {
         // Move the target to a new spot
-        Target.localPosition = new Vector3(Random.value * 34 - 17, Target.localPosition.y, Target.localPosition.z) ;
+        Target.localPosition = new Vector3(Random.value * 34 - 17, Target.localPosition.y, Target.localPosition.z);
+        reachedGoal = false;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -68,40 +93,44 @@ public class WalkerAgentMulti : Agent
         // Target and Agent positions
         sensor.AddObservation(Target.localPosition.x);
         sensor.AddObservation(Target.localPosition.z);
-        sensor.AddObservation(this.transform.localPosition.x);
-        sensor.AddObservation(this.transform.localPosition.z);
+        sensor.AddObservation(transform.localPosition.x);
+        sensor.AddObservation(transform.localPosition.z);
         // Agent direction (useful to understand ray perception)
         //sensor.AddObservation(this.transform.rotation.eulerAngles.y);
     }
 
     public float playerSpeed = 10;
+    public bool reachedGoal;
+
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Actions, size = 2
         Vector3 direction = Vector3.zero;
         direction.x = actionBuffers.ContinuousActions[0];
         direction.z = actionBuffers.ContinuousActions[1];
+
         controller.Move(direction * Time.deltaTime * playerSpeed);
         if (direction != Vector3.zero)
         {
-            this.transform.forward = direction;
+            transform.forward = direction;
         }
 
         // Rewards
-        float distanceToTarget = Vector3.Distance(this.transform.position, Target.position);
 
         // Reached target
-        if (distanceToTarget < 1.42f)
+        if (reachedGoal)
         {
-            Transform parent = this.transform.parent;
-            for(int i = 0; i < parent.childCount; i++)
+            Transform parent = transform.parent;
+            for (int i = 0; i < parent.childCount; i++)
             {
                 GameObject Go = parent.GetChild(i).gameObject;
-                if (Go.TryGetComponent<WalkerAgentMulti>(out WalkerAgentMulti agentm)){
+                if (Go.TryGetComponent(out WalkerAgentMulti agentm))
+                {
                     agentm.SetReward(1.0f);
                     agentm.EndEpisode();
                 }
             }
+
             MoveTarget();
         }
         else
@@ -109,6 +138,31 @@ public class WalkerAgentMulti : Agent
             AddReward(-0.0001f);
         }
 
+        //todo Aless code, instead of checking distance; checking if we got to the goal then restart the episode
+
+        // float distanceToTarget = Vector3.Distance(transform.position, Target.position);
+        //
+        // // Reached target
+        // if (distanceToTarget < 1.42f)
+        // {
+        //     print("reached goal"); // you can use print for some scripts, for others you need Debug.Log
+        //     Transform parent = transform.parent;
+        //     for (int i = 0; i < parent.childCount; i++)
+        //     {
+        //         GameObject Go = parent.GetChild(i).gameObject;
+        //         if (Go.TryGetComponent<WalkerAgentMulti>(out WalkerAgentMulti agentm))
+        //         {
+        //             agentm.SetReward(1.0f);
+        //             agentm.EndEpisode();
+        //             rb.velocity = Vector3.zero;
+        //         }
+        //     }
+        //     MoveTarget();
+        // }
+        // else
+        // {
+        //     AddReward(-0.0001f);
+        // }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
