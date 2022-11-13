f1ef45b4ace2c610c68d84f096637f70ebc87b8f
Toader Carausu <tcarausu@gmail.com>
2022-11-13 15:45:16 +0100

    adjusted the scripts + added tag check on goal

---
+++ Assets/Scripts/GoalCheckScript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCheckScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("agent"))
+        {
+            print("goal yes yes");
+            other.gameObject.GetComponent<WalkerAgentMulti>().reachedGoal = true;
+        }
+    }
+}
