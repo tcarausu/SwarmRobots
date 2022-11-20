00ffaf85e2b6ac25b9c4f83ea81e8a1cfc7574b7
Giovanni Giacometti <giovanni1.giacometti@mail.polimi.it>
2022-11-20 22:59:36 +0100

    Add new trained model

--- envs/src/__init__.py
+++ envs/src/__init__.py
@@ -1,6 +1,7 @@
 from src.experience import Experience
-from src.trainer import Trainer
+from src.trainer_singleagent import TrainerSingleAgent
+from src.trainer_multiagent import TrainerMultiAgent
 from src.neuroevolution import NeuroAgent,NeuroModel
 from src.ddpg import DDPG
 
-__all__ = ['Experience', 'Trainer', 'NeuroAgent','NeuroModel', 'DDPG']
+__all__ = ['Experience', 'TrainerSingleAgent', 'NeuroAgent','NeuroModel', 'DDPG','TrainerMultiAgent']
