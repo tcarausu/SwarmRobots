e9ab044e575b843fb16a813b0e676cbd4e60e216
Toader Carausu <tcarausu@gmail.com>
2022-11-30 18:01:05 +0100

    multi with discrete actions

--- Assets/Scripts/WalkerAgentMulti.cs
+++ Assets/Scripts/WalkerAgentMulti.cs
@@ -10,8 +10,11 @@ public class WalkerAgentMulti : Agent
     public enum Movement
     {
         UpAndRight,
-        ForwardAndRotate
+        ForwardAndRotate,
+        DiscreteActions
     }
+
+    private Rigidbody m_AgentRb;
 
     public Movement mode;
     public float playerSpeed = 10;
@@ -30,6 +33,9 @@ public class WalkerAgentMulti : Agent
     public float targetReward = 1;
     public float nearTargetReward = 0.01f;
 
+    private int numEpisodes = 0;
+    public int changePosTarget = 10;
+
     // Retrieved from the training area, remember to set the observation size correctly in the prefab
     public bool useCommunication;
 
@@ -44,6 +50,8 @@ public class WalkerAgentMulti : Agent
 
     private List<Checkpoint> clearedCheckpoints;
 
+    private Vector3 targetInitialPosition;
+
     private Transform MySpawnArea;
     private SpawnCheck spawnCheck;
     private Vector3 initialPosition;
@@ -55,6 +63,8 @@ public class WalkerAgentMulti : Agent
     private Transform Goal;
 
     public bool hasRandomMaze;
+
+    [HideInInspector] public bool seenTarget = false;
 
     void Start()
     {
@@ -69,6 +79,8 @@ public class WalkerAgentMulti : Agent
         length = GetComponent<BoxCollider>().size.x;
         spawnSize.x -= length / 2;
         spawnSize.z -= length / 2;
+
+        m_AgentRb = GetComponent<Rigidbody>();
 
         if (hasRandomMaze)
             activateTheMaze();
@@ -136,7 +148,7 @@ public class WalkerAgentMulti : Agent
         int obsSize = 0;
         if (useCommunication)
         {
-            obsSize = otherAgents.Count + 4; //adding 4 because of the localpos+rotationY
+            obsSize = otherAgents.Count + 3; //adding 4 because of the localpos+rotationY
             communicationList = new List<float>(new float[obsSize]);
         }
 
@@ -172,8 +184,18 @@ public class WalkerAgentMulti : Agent
 
     private void MoveTarget()
     {
-        // Move the target to a new spot
-        Target.localPosition = TargetSpawnAreas.GetRndPosition();
+        // // Move the target to a new spot
+        // Target.localPosition = TargetSpawnAreas.GetRndPosition();
+
+        //comment this instead of the row above
+        if (numEpisodes % changePosTarget == 0)
+        {
+            Target.localPosition = TargetSpawnAreas.GetRndPosition();
+            targetInitialPosition = Target.localPosition;
+        }
+        else
+            Target.localPosition = targetInitialPosition;
+
         reachedGoal = false;
     }
 
@@ -184,6 +206,10 @@ public class WalkerAgentMulti : Agent
         {
             CheckNearAgentsAndUpdateCommunication();
             sensor.AddObservation(communicationList);
+
+            sensor.AddObservation(transform.localPosition.x);
+            sensor.AddObservation(transform.localPosition.z);
+            // sensor.AddObservation(transform.rotation.eulerAngles.y);
         }
         else
             CheckNearAgents();
@@ -194,9 +220,9 @@ public class WalkerAgentMulti : Agent
         //sensor.AddObservation(transform.localPosition.x);
         //sensor.AddObservation(transform.localPosition.z);
         // Agent direction (useful to understand ray perception)
-        
-        sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(transform.rotation.eulerAngles.y);
+
+        // sensor.AddObservation(transform.localPosition);
+        // sensor.AddObservation(transform.rotation.eulerAngles.y);
     }
 
     private bool reachedGoal;
@@ -208,7 +234,41 @@ public class WalkerAgentMulti : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        if (mode.Equals(Movement.UpAndRight))
+        //todo this is with discrete actions
+
+        if (mode.Equals(Movement.DiscreteActions))
+        {
+            var dirToGo = Vector3.zero;
+            var rotateDir = Vector3.zero;
+
+            var action = actionBuffers.DiscreteActions[0];
+
+            switch (action)
+            {
+                case 1:
+                    dirToGo = transform.forward * 1f;
+                    break;
+                case 2:
+                    dirToGo = transform.forward * -1f;
+                    break;
+                case 3:
+                    rotateDir = transform.up * 1f;
+                    break;
+                case 4:
+                    rotateDir = transform.up * -1f;
+                    break;
+                case 5:
+                    dirToGo = transform.right * -0.75f;
+                    break;
+                case 6:
+                    dirToGo = transform.right * 0.75f;
+                    break;
+            }
+
+            transform.Rotate(rotateDir * rotationSensitivity);
+            controller.Move(dirToGo * Time.deltaTime * playerSpeed);
+        }
+        else if (mode.Equals(Movement.UpAndRight))
         {
             Vector3 direction = Vector3.zero;
             direction.x = actionBuffers.ContinuousActions[0];
@@ -234,11 +294,40 @@ public class WalkerAgentMulti : Agent
             return;
         }
 
-        AddReward(-existenctialPenalty);
-        Vector3 dir = (Target.position - transform.position).normalized;
-        if (Physics.Raycast(transform.position, dir, out RaycastHit hit))
-            if (hit.collider.name.Equals(Target.name))
-                AddReward((1 / hit.distance) * nearTargetReward);
+        //todo this is with continous
+        // if (mode.Equals(Movement.UpAndRight))
+        // {
+        //     Vector3 direction = Vector3.zero;
+        //     direction.x = actionBuffers.ContinuousActions[0];
+        //     direction.z = actionBuffers.ContinuousActions[1];
+        //
+        //     controller.Move(playerSpeed * Time.deltaTime * direction);
+        //     if (direction != Vector3.zero)
+        //         transform.forward = direction;
+        // }
+        // else if (mode.Equals(Movement.ForwardAndRotate))
+        // {
+        //     var rotation = actionBuffers.ContinuousActions[0];
+        //     var forwardMovement = actionBuffers.ContinuousActions[1];
+        //
+        //     controller.Move(forwardMovement * playerSpeed * Time.deltaTime * transform.forward);
+        //     transform.Rotate(rotation * rotationSensitivity * Vector3.up);
+        // }
+        //
+        //
+        // if (reachedGoal)
+        // {
+        //     ReachedTarget();
+        //     return;
+        // }
+        //
+        // AddReward(-existenctialPenalty);
+        // Vector3 dir = (Target.position - transform.position).normalized;
+        // if (Physics.Raycast(transform.position, dir, out RaycastHit hit))
+        //     if (hit.collider.name.Equals(Target.name))
+        //         AddReward((1 / hit.distance) * nearTargetReward);
+        //
+        //
     }
 
     // Set the reward of all agents to 1, End the episode and move the target
@@ -246,8 +335,12 @@ public class WalkerAgentMulti : Agent
     {
         SetReward(targetReward);
         EndEpisode();
+
+        numEpisodes += 1;
+
         foreach (WalkerAgentMulti agent in otherAgents)
         {
+            agent.numEpisodes += 1;
             agent.SetReward(targetReward);
             agent.EndEpisode();
         }
@@ -256,6 +349,7 @@ public class WalkerAgentMulti : Agent
             activateTheMaze();
         else
             useExisitingMaze();
+
         MoveTarget();
     }
 
@@ -275,8 +369,11 @@ public class WalkerAgentMulti : Agent
     // Near agent penalty and update communicationList
     private void CheckNearAgentsAndUpdateCommunication()
     {
+        AddReward(-existenctialPenalty);
+
         Vector3 dir;
         communicationList.Clear();
+
         foreach (Agent agent in otherAgents)
         {
             dir = (agent.transform.position - transform.position).normalized;
@@ -291,12 +388,66 @@ public class WalkerAgentMulti : Agent
                     communicationList.Add(0);
             }
         }
+
+        Vector3 dirT = (Target.position - transform.position).normalized;
+        if (Physics.Raycast(transform.position, dirT, out RaycastHit hitT))
+        {
+            if (hitT.collider.name.Equals(Target.name))
+            {
+                AddReward((1 / hitT.distance) * nearTargetReward);
+                communicationList.Add(Target.localPosition.x);
+                communicationList.Add(Target.localPosition.z);
+                if (!seenTarget) //first time it is seen
+                {
+                    seenTarget = true;
+                    foreach (WalkerAgentMulti agent in otherAgents) 
+                        agent.seenTarget = true;
+                }
+            }
+            else
+            {
+                if (seenTarget)
+                {
+                    communicationList.Add(Target.localPosition.x);
+                    communicationList.Add(Target.localPosition.z);
+                }
+                else
+                {
+                    communicationList.Add(0);
+                    communicationList.Add(0);
+                }
+            }
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        if (mode.Equals(Movement.DiscreteActions))
+        {
+            var discreteActionsOut = actionsOut.DiscreteActions;
+            if (Input.GetKey(KeyCode.D))
+            {
+                discreteActionsOut[0] = 3;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                discreteActionsOut[0] = 1;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                discreteActionsOut[0] = 4;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                discreteActionsOut[0] = 2;
+            }
+        }
+
+        else
+        {
+            var continuousActionsOut = actionsOut.ContinuousActions;
+            continuousActionsOut[0] = Input.GetAxis("Horizontal");
+            continuousActionsOut[1] = Input.GetAxis("Vertical");
+        }
     }
 }
