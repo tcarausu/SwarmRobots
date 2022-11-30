06d4d93211bec5918ba83748abed5a063e93feea
Giovanni Giacometti <giovanni1.giacometti@mail.polimi.it>
2022-11-30 08:28:06 +0100

    discrete actions

--- Assets/Scripts/WalkerAgentMulti.cs
+++ Assets/Scripts/WalkerAgentMulti.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Security.Cryptography;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Policies;
@@ -14,8 +15,11 @@ public class WalkerAgentMulti : Agent
     public enum Movement
     {
         UpAndRight,
-        ForwardAndRotate
+        ForwardAndRotate,
+        DiscreteActions
     }
+
+    private Rigidbody m_AgentRb;
 
     public Movement mode;
     public float playerSpeed = 10;
@@ -36,6 +40,7 @@ public class WalkerAgentMulti : Agent
 
     private int numEpisodes = 0;
     public int changePosTarget = 10;
+
 
     // Retrieved from the training area, remember to set the observation size correctly in the prefab
     public bool useCommunication;
@@ -82,7 +87,7 @@ public class WalkerAgentMulti : Agent
         spawnSize.x -= length / 2;
         spawnSize.z -= length / 2;
 
-        
+        m_AgentRb = GetComponent<Rigidbody>();
 
         if (hasRandomMaze)
             activateTheMaze();
@@ -236,7 +241,41 @@ public class WalkerAgentMulti : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        if (mode.Equals(Movement.UpAndRight))
+        if (mode.Equals(Movement.DiscreteActions))
+        {
+            
+            var dirToGo = Vector3.zero;
+            var rotateDir = Vector3.zero;
+
+            var action = actionBuffers.DiscreteActions[0];
+            Debug.Log(action);
+            Debug.Log(transform.forward);
+
+            switch (action)
+            {
+                case 1:
+                    dirToGo = transform.forward * 1f;
+                    break;
+                case 2:
+                    dirToGo = transform.forward * -1f;
+                    break;
+                case 3:
+                    rotateDir = transform.up * 1f;
+                    break;
+                case 4:
+                    rotateDir = transform.up * -1f;
+                    break;
+                case 5:
+                    dirToGo = transform.right * -0.75f;
+                    break;
+                case 6:
+                    dirToGo = transform.right * 0.75f;
+                    break;
+            }
+            transform.Rotate(rotateDir * rotationSensitivity);
+            controller.Move(dirToGo * Time.deltaTime * playerSpeed);
+        }
+        else if (mode.Equals(Movement.UpAndRight))
         {
             Vector3 direction = Vector3.zero;
             direction.x = actionBuffers.ContinuousActions[0];
@@ -246,7 +285,7 @@ public class WalkerAgentMulti : Agent
             if (direction != Vector3.zero)
                 transform.forward = direction;
         }
-        else if (mode.Equals(Movement.ForwardAndRotate))
+        else if(mode.Equals(Movement.ForwardAndRotate))
         {
             var rotation = actionBuffers.ContinuousActions[0];
             var forwardMovement = actionBuffers.ContinuousActions[1];
@@ -254,6 +293,7 @@ public class WalkerAgentMulti : Agent
             controller.Move(forwardMovement * playerSpeed * Time.deltaTime * transform.forward);
             transform.Rotate(rotation * rotationSensitivity * Vector3.up);
         }
+        
 
 
         if (reachedGoal)
@@ -365,8 +405,32 @@ public class WalkerAgentMulti : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        if (mode.Equals(Movement.DiscreteActions))
+        {
+            var discreteActionsOut = actionsOut.DiscreteActions;
+            if (Input.GetKey(KeyCode.D))
+            {
+                discreteActionsOut[0] = 3;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                discreteActionsOut[0] = 1;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                discreteActionsOut[0] = 4;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                discreteActionsOut[0] = 2;
+            }
+        }
+
+        else { 
+                                    
+            var continuousActionsOut = actionsOut.ContinuousActions;
+            continuousActionsOut[0] = Input.GetAxis("Horizontal");
+            continuousActionsOut[1] = Input.GetAxis("Vertical");
+        }
     }
 }
