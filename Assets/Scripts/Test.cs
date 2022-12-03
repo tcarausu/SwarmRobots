542eb66ad1d3be12923d69648d8dd694974c58bf
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2022-12-03 17:38:40 +0100

    Refactor code for Test and multiagent
    Added different features and improved usability via Unity inspector

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,22 +1,40 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Policies;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Test : Agent
 {
-    public enum Movement { UpAndRight, ForwardAndRotate }
-    public Movement mode;
-    public float playerSpeed = 20;
-    public float rotationSensitivity = 10;
+    public enum Movement
+    {
+        NSWE,
+        ForwardAndRotate,
+        DiscreteActions
+    }
 
+    public enum Communication
+    {
+        Absent,
+        Distance,
+        FreeCommunication,
+        TargetPosition
+    }
+
+    public List<Communication> CommunicationMode;
+    public Movement movementMode;
+    public float playerSpeed = 10;
+    public float rotationSensitivity = 10;
+    public bool ownPositionAsObservation;
+
+    [Header("Rewards")]
     public float existenctialPenalty = 0.001f;
+
     [Tooltip("The penalty is multiplied by 1/distance from the near agent if they are on sigth")]
-    [Header("Inversely proportional to distance")]
     public float nearAgentPenalty = 0.1f;
+
     public float hitWallPenalty = 0.05f;
     public float hitAgentPenalty = 0.03f;
 
@@ -24,74 +42,84 @@ public class Test : Agent
     public float targetReward = 1;
     public float nearTargetReward = 0.01f;
 
-    public bool useCommunication;
-    private List<float> communicationList;
+    private Dictionary<string, float> communicationMap;
 
     private Transform Swarm;
     private List<Test> otherAgents;
     private Transform Target;
-//    private SpawnAreas TargetSpawnAreas;
 
     private CharacterController controller;
-
-//    private List<Checkpoint> clearedCheckpoints;
-
-//    private Transform MySpawnArea;
-//    private SpawnCheck spawnCheck;
+    private List<Checkpoint> clearedCheckpoints;
     private Vector3 initialPosition;
-//    private Vector3 spawnSize;
-//    private float length; // Since the agent is a cube edge length on the x is supposed to be length on y and z too
 
+    [Header("Test parameters")]
+    public List<Vector3> TargetPositions;
     private int testNumber = 0;
     private System.DateTime startTime;
-    public List<Vector3> TargetPositions; 
     public int maxMoves;
     private int moves = 0;
+    private float totalReward;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
- //       clearedCheckpoints = new List<Checkpoint>();
+        clearedCheckpoints = new List<Checkpoint>();
 
         Swarm = transform.parent;
- //       spawnCheck = Swarm.gameObject.GetComponent<SpawnCheck>();
- //       MySpawnArea = Swarm.Find("SpawnArea");
         initialPosition = transform.localPosition;
- //       spawnSize = MySpawnArea.GetComponent<Renderer>().bounds.size;
- //       length = GetComponent<BoxCollider>().size.x;
- //       spawnSize.x -= length / 2;
- //       spawnSize.z -= length / 2;
-
         Transform Goal = Swarm.parent.Find("GOAL");
- //       GameObject go = Goal.Find("SpawnAreas").gameObject;
- //       TargetSpawnAreas = go.GetComponent<SpawnAreas>();
         Target = Goal.Find("Target");
 
         otherAgents = new List<Test>(Swarm.GetComponentsInChildren<Test>());
         otherAgents.Remove(this);
 
-        // Compute observation size based on the useCommunication parameter
         int obsSize = 0;
-        if (useCommunication)
+        communicationMap = new Dictionary<string, float>();
+        //Initialize the communication data structure
+        if (CommunicationMode.Contains(Communication.Distance))
         {
-            obsSize = otherAgents.Count;
-            communicationList = new List<float>(new float[(int)(obsSize)]);
+            obsSize += otherAgents.Count;
+            foreach (var agent in otherAgents)
+                communicationMap.Add(agent.name + "distance", 0);
         }
+        if (CommunicationMode.Contains(Communication.FreeCommunication))
+        {
+            obsSize += otherAgents.Count;
+            foreach (var agent in otherAgents)
+                communicationMap.Add(agent.name + "freeCommunication", 0);
+        }
+        if (CommunicationMode.Contains(Communication.TargetPosition))
+        {
+            obsSize += 2;
+            communicationMap.Add("targetx", 0);
+            communicationMap.Add("targetz", 0);
+        }
+
+        if (ownPositionAsObservation)
+            obsSize += 2;
 
         // Since I can't set the size from here I just notify the user
         Debug.Assert(GetComponent<BehaviorParameters>().BrainParameters.VectorObservationSize == obsSize,
-            "Wrong observation size, change it from the prefab of the Agent. Actual value = " +
-            GetComponent<BehaviorParameters>().BrainParameters.VectorObservationSize + " but expected " + obsSize +
-            "\nRemember you have Communication set to " + useCommunication);
+        "Wrong observation size, change it from the prefab of the Agent. Actual value = " +
+        GetComponent<BehaviorParameters>().BrainParameters.VectorObservationSize + " but expected " + obsSize +
+        "\nRemember you have Communication set to " + CommunicationMode.ToString());
+
 
         startTime = System.DateTime.UtcNow;
         moves = 0;
+        totalReward = 0;
+    }
+
+    private new void AddReward(float reward)
+    {
+        totalReward += reward;
+        base.AddReward(reward);
     }
 
     public void Checkpoint(Checkpoint cp)
     {
         AddReward(checkpointReward);
- //       clearedCheckpoints.Add(cp);
+        clearedCheckpoints.Add(cp);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -107,19 +135,20 @@ public class Test : Agent
 
     public override void OnEpisodeBegin()
     {
-        //foreach (Checkpoint cp in clearedCheckpoints)
-        //    cp.SetActive(true);
+        foreach (Checkpoint cp in clearedCheckpoints)
+            cp.SetActive(true);
 
-        //clearedCheckpoints.Clear();
+        clearedCheckpoints.Clear();
+
         ResetPosition();
         testNumber++;
 
-        if (testNumber-1 == TargetPositions.Count)
+        if (testNumber - 1 == TargetPositions.Count)
         {
             Application.Quit();
             UnityEditor.EditorApplication.isPlaying = false;
         }
-        
+
         startTime = System.DateTime.UtcNow;
         moves = 0;
     }
@@ -129,99 +158,147 @@ public class Test : Agent
         transform.localPosition = initialPosition;
     }
 
-    //private void RndSpawn()
-    //{
-    //    Vector3 rndPosition;
-    //    do
-    //    {
-    //        rndPosition = new Vector3(
-    //        Random.value * spawnSize.x - spawnSize.x / 2,
-    //        initialPosition.y,
-    //        Random.value * spawnSize.z - spawnSize.z / 2);
-    //    }
-    //    while (!spawnCheck.IsSafePosition(rndPosition, length));
-
-    //    transform.localPosition = rndPosition;
-
-    //}
-
     private void MoveTarget()
     {
         // Move the target to a new spot
-        if(testNumber - 1 < TargetPositions.Count)
-            Target.localPosition = TargetPositions[testNumber-1];
+        if (testNumber - 1 < TargetPositions.Count)
+            Target.localPosition = TargetPositions[testNumber - 1];
         reachedGoal = false;
+    }
+
+    public void Communicate(string id, float message)
+    {
+        communicationMap[id] = message;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
 
-        //The only observation is the raycast if communication is not used
-        if (useCommunication)
-        {
+        if (CommunicationMode.Contains(Communication.Distance))
             CheckNearAgentsAndUpdateCommunication();
-            sensor.AddObservation(communicationList);
-        }
         else
             CheckNearAgents();
 
-        // Target and Agent positions
-        //sensor.AddObservation(Target.localPosition.x);
-        //sensor.AddObservation(Target.localPosition.z);
-        //sensor.AddObservation(transform.localPosition.x);
-        //sensor.AddObservation(transform.localPosition.z);
-        // Agent direction (useful to understand ray perception)
-        //sensor.AddObservation(this.transform.rotation.eulerAngles.y);
+        //The only observation is the raycast if communication is not used
+        if (CommunicationMode.Count() != 0 && !CommunicationMode.Contains(Communication.Absent))
+            sensor.AddObservation(communicationMap.Values.ToList());
+
+        if (ownPositionAsObservation)
+        {
+            sensor.AddObservation(transform.localPosition.x);
+            sensor.AddObservation(transform.localPosition.z);
+        }
+
     }
 
     private bool reachedGoal;
     public void ReachGoal() { reachedGoal = true; }
-
+    private void CommunicateTargetPosition()
+    {
+        foreach (Test agent in otherAgents)
+        {
+            agent.Communicate("targetx", Target.localPosition.x);
+            agent.Communicate("targetz", Target.localPosition.z);
+        }
+    }
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        if (mode.Equals(Movement.UpAndRight))
+        switch (movementMode)
         {
-            Vector3 direction = Vector3.zero;
-            direction.x = actionBuffers.ContinuousActions[0];
-            direction.z = actionBuffers.ContinuousActions[1];
+            case Movement.NSWE:
+                Vector3 direction = Vector3.zero;
+                direction.x = actionBuffers.ContinuousActions[0];
+                direction.z = actionBuffers.ContinuousActions[1];
 
-            controller.Move(playerSpeed * Time.deltaTime * direction);
-            if (direction != Vector3.zero)
-                transform.forward = direction;
-        }
-        else if (mode.Equals(Movement.ForwardAndRotate))
-        {
-            var rotation = actionBuffers.ContinuousActions[0];
-            var forwardMovement = actionBuffers.ContinuousActions[1];
+                controller.Move(playerSpeed * Time.deltaTime * direction);
+                if (direction != Vector3.zero)
+                    transform.forward = direction;
+                break;
 
-            controller.Move(forwardMovement * playerSpeed * Time.deltaTime * transform.forward);
-            transform.Rotate(rotation * rotationSensitivity * Vector3.up);
+            case Movement.ForwardAndRotate:
+                var rotation = actionBuffers.ContinuousActions[0];
+                var forwardMovement = actionBuffers.ContinuousActions[1];
+
+                controller.Move(forwardMovement * playerSpeed * Time.deltaTime * transform.forward);
+                transform.Rotate(rotation * rotationSensitivity * Vector3.up);
+                break;
+
+            case Movement.DiscreteActions:
+                var dirToGo = Vector3.zero;
+                var rotateDir = Vector3.zero;
+
+                var action = actionBuffers.DiscreteActions[0];
+
+                switch (action)
+                {
+                    case (int)DiscreteCommands.Forward:
+                        dirToGo = transform.forward * 1f;
+                        break;
+                    case (int)DiscreteCommands.Backward:
+                        dirToGo = transform.forward * -1f;
+                        break;
+                    case (int)DiscreteCommands.TurnLeft:
+                        rotateDir = transform.up * 1f;
+                        break;
+                    case (int)DiscreteCommands.TurnRight:
+                        rotateDir = transform.up * -1f;
+                        break;
+                    case (int)DiscreteCommands.Left:
+                        dirToGo = transform.right * -0.75f;
+                        break;
+                    case (int)DiscreteCommands.Right:
+                        dirToGo = transform.right * 0.75f;
+                        break;
+                }
+                transform.Rotate(rotateDir * rotationSensitivity);
+                controller.Move(playerSpeed * Time.deltaTime * dirToGo);
+                break;
         }
 
         if (reachedGoal)
         {
             Debug.Log("Reached Target in test number " + testNumber);
             System.TimeSpan ts = System.DateTime.UtcNow - startTime;
-            Debug.Log("Time needed to reach: " + (ts.TotalMilliseconds/1000.0f).ToString());
+            Debug.Log("Time needed to reach: " + (ts.TotalMilliseconds / 1000.0f).ToString() + "  ---  Total reward = " + totalReward);
             ReachedTarget();
             return;
         }
 
+        AddReward(-existenctialPenalty);
+
+        CheckTargetProximity();
+
         moves++;
-        if(moves >= maxMoves)
+        if (moves >= maxMoves)
         {
             System.TimeSpan ts = System.DateTime.UtcNow - startTime;
-            Debug.Log("Failed test number " + testNumber + " after " + (ts.TotalMilliseconds / 1000.0f).ToString() + " seconds");
+            Debug.Log("Failed test number " + testNumber + " after " + (ts.TotalMilliseconds / 1000.0f).ToString() + " seconds" + "  ---  Total reward = " + totalReward);
             ReachedTarget();
             return;
         }
-        
-        AddReward(-existenctialPenalty);
+
+        if (CommunicationMode.Contains(Communication.FreeCommunication))
+        {
+            int messageIndex = actionBuffers.ContinuousActions.Length - 1;
+            foreach (Test agent in otherAgents)
+            {
+                float message = actionBuffers.ContinuousActions[messageIndex];
+                agent.Communicate(name + "freeCommunication", message);
+            }
+        }
+
+    }
+
+    private void CheckTargetProximity()
+    {
         Vector3 dir = (Target.position - transform.position).normalized;
         if (Physics.Raycast(transform.position, dir, out RaycastHit hit))
             if (hit.collider.name.Equals(Target.name))
+            {
                 AddReward((1 / hit.distance) * nearTargetReward);
-
+                if (CommunicationMode.Contains(Communication.TargetPosition))
+                    CommunicateTargetPosition();
+            }
     }
 
     // Set the reward of all agents to 1, End the episode and move the target
@@ -255,7 +332,6 @@ public class Test : Agent
     private void CheckNearAgentsAndUpdateCommunication()
     {
         Vector3 dir;
-        communicationList.Clear();
         foreach (Agent agent in otherAgents)
         {
             dir = (agent.transform.position - transform.position).normalized;
@@ -264,18 +340,39 @@ public class Test : Agent
                 if (hit.collider.name.Equals(agent.name))
                 {
                     AddReward(-(1 / hit.distance) * nearAgentPenalty);
-                    communicationList.Add(hit.distance);
+                    communicationMap[agent.name + "distance"] = hit.distance;
                 }
                 else
-                    communicationList.Add(0);
+                    communicationMap[agent.name + "distance"] = 0;
             }
         }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        var discreteActionsOut = actionsOut.DiscreteActions;
         var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        switch (movementMode)
+        {
+            case Movement.DiscreteActions:
+                if (Input.GetKey(KeyCode.D))
+                    discreteActionsOut[0] = (int)DiscreteCommands.Right;
+                else if (Input.GetKey(KeyCode.W))
+                    discreteActionsOut[0] = (int)DiscreteCommands.Forward;
+                else if (Input.GetKey(KeyCode.A))
+                    discreteActionsOut[0] = (int)DiscreteCommands.Left;
+                else if (Input.GetKey(KeyCode.S))
+                    discreteActionsOut[0] = (int)DiscreteCommands.Backward;
+                break;
+
+            default:
+                continuousActionsOut = actionsOut.ContinuousActions;
+                continuousActionsOut[0] = Input.GetAxis("Horizontal");
+                continuousActionsOut[1] = Input.GetAxis("Vertical");
+                break;
+        }
+
+        if (CommunicationMode.Contains(Communication.FreeCommunication))
+            continuousActionsOut[continuousActionsOut.Length - 1] = 0;
     }
 }
