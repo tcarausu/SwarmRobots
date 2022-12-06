05397f947375b6faef67f362d0ea41bcf005a3da
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2022-12-06 02:22:35 +0100

    Show free communication log in real time

--- showLog.py
+++ showLog.py
@@ -1,19 +1,33 @@
+from matplotlib import animation
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, PillowWriter
 
-fig,ax = plt.subplots(1,1)
-scatter_positions = ax.scatter([], [])
+import time
+
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.set_xlim([0, 2000])
+ax.set_ylim([-1, 1])
+
+
+line, = ax.plot([],[],'b-', animated=True)
+# getFrame() makes a call to the CCD frame buffer and retrieves the most recent frame
+
+# animation function
 count = 0
 def update(frame):
-    count += 1
-    print(count)
-    
+    global count
     with open('Communication_Test.txt', 'r') as f:
-        comm = [float(line[:-1].replace(",",".")) for line in f]
-    scatter_positions.set_offsets([list(range(len(comm))),comm])
-    return scatter_positions
+        comm = [float(line[:-1].replace(",",".")) for line in f][:count]
+        count+=1
+    line.set_xdata(list(range(len(comm))))
+    line.set_ydata(comm)
+    ax.set_xlim([max(len(comm)-2000, 0), max(2000, len(comm)+10)])
+    return line,
 
-anim = FuncAnimation(fig, update, frames=500, interval = 40, repeat = False)
-update(0)
+
+# call the animator
+anim = animation.FuncAnimation(fig, update, interval=1000, blit=True)
 plt.show()
-anim = 0
